5e65f94d015283ad6f0d86c95ed66ed4d48b5197
santoshr04 <san.invent@gmail.com>
2026-08-03 11:44:24 +0530

    26-08-03_1144_Ruthvi-commit working

---
+++ login_app/app/src/main/kotlin/com/example/loginapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+        webView.webViewClient = WebViewClient()
+        
+        val webSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true
+
+        webView.loadUrl("http://10.0.7.40:8080/Auth/Login")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
